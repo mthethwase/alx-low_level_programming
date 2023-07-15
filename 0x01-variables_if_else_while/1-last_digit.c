2932eb11aedd701641746792094d0d9d38235511
mthethwase <mthethwase4@gmail.com>
2023-07-15 07:18:44 -0700

    1-last_digit.c

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,36 +1,30 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
-/* more headers goes there */
-
-/* betty style doc for function main goes there */
-
 /**
  * main - Entry point
- *
- * Return: Always 0 (success)
+ * Description: 'get the degit of a number'
+ * Return: always 0
  */
-
-
 int main(void)
 {
 	int n;
-	int lastnum;
+	int ld;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	lastnum = n % 10;
-	if (lastnum > 5)
-{
-	printf("Last digit of %d is %d and is greater than 5\n", n' lastnum);
-}
-	else if (lastnum == 0)
-{
-	printf("Last digit of %d is %d and is 0\n", n, lastnum);
-}
-	else if (lastnum < 6 && lastnum !=0)
-{
-	printf("Last digit of %d is %d and is less than 6 and not 0\n", n' lastnum);
-}
+	ld = n % 10;
+	if (ld > 5)
+	{
+		printf("Last digit of %d is %d and greater than 5", n, ld);
+	}
+	else if (ld == 0)
+	{
+		printf("Last digit of %d is %d and is 0", n, ld);
+	}
+	else
+	{
+		printf("Last digit of %d is %d and less 6 and not 0", n, ld);
+	}
 	return (0);
 }
