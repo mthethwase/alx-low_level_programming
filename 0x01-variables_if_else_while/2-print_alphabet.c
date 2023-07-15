fd4a271b9d5e4edf267867dde4e5ad8ce143d6d4
mthethwase <mthethwase4@gmail.com>
2023-07-15 06:46:08 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <ctype.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always ) (success)
+ */
+
+int main(void)
+{
+int lowerCase = 'a';
+while (lowerCase <= 'z')
+{
+putchar(lowerCase);
+lowercase += 1;
+}
+putchar('\n');
+return (0);
+}
