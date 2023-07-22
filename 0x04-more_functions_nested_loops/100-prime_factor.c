cb877a1c5b663231759a8a7a444c2d35029df8f9
mthethwase <mthethwase4@gmail.com>
2023-07-22 08:32:24 -0700

    100-prime_factor.c

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+/**
+ * main - print the largesr prime factor of the
+ * number 612852475143
+ * Return: (0)
+ */
+int main(void)
+{
+	long int i, n, d;
+
+	n = 612852475143;
+	for (i = 1; i <= n; i++)
+	{
+		if (n % i == 0)
+		{
+			if (n == 1)
+			{
+				printf("%ld\n", i);
+				break;
+			}
+			d = n / i;
+			n = d;
+		}
+	}
+
+	return (0);
+}
