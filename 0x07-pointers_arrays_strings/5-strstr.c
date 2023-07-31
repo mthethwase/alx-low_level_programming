a9981041af36431503cf4bc20e1682ed35db815e
mthethwase <mthethwase4@gmail.com>
2023-07-31 13:42:07 -0700

    7-print_chessboard.c

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * print_chessboard - Entry point
+ * @a: array
+ * Return: Always 0 (Success)
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	int i;
+	int j;
+
+	for (i
